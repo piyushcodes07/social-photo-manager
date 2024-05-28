4bb26a69f05feade409373f716431afe2ebbf6d9
piyushcodes07 <piyushcodes07@gmail.com>
2024-05-28 21:14:02 +0530

    v2

--- app/components/uploader.tsx
+++ app/components/uploader.tsx
@@ -16,7 +16,7 @@ export default function AvatarUploadPage() {
   
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4">Upload Your Image</h1>
+      <h1 className="text-3xl font-bold mb-4">Upload Image</h1>
 
       <form
         onSubmit={async (event) => {
